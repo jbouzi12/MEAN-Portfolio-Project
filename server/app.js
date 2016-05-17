5673bea1b2e4bd1af70708f4a49b735d245f52c4
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-05-17 16:12:54 -0400

    update tests to test search response length and organize test structure

--- server/app.js
+++ server/app.js
@@ -3,11 +3,21 @@ var express		= require('express'),
 	mongoose	= require('mongoose'),
 	bodyParser	= require('body-parser'),
 	methodOverride = require('method-override'),
-	port 		= process.env.PORT || 8080;
+	port 		= process.env.PORT || 8080,
+	request 	= require('request'),
+	querystring = require('querystring'),
+	cookieParser 	= require('cookie-parser');
+	client_id 		= 'CLIENT_ID',
+	client_secret 	= 'CLIENT_SECRET',
+	redirect_url  	= 'REDIRECT_URL',
+	scopes 			= 'user-read-private user-read-email';
 
-app.use(bodyParser.json()); // parse application/json 
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+
+app.use(bodyParser.json()); 
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 app.use(function(req, res, next){ // enable CORS (will refactor into middleware later)
 	res.header('Access-Control-Allow-Origin', "*");
 	res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -6,30 +6,55 @@ var chai 		= require('chai'),
 chai.use(chaiHttp);
 
 describe('Search', function(){
+	it('should return a 200 status code', function(done){
+		chai.request(server)
+		.get('/search')
+		.end(function(err, res){
+			res.should.have.status(200);
+			done();
+		})
+	});
+
+	it('should return a list of N results', function(done){
+		var n = 5;
+		chai.request(server)
+		.get('/search')
+		.query({limit:n})
+		.query({type: 'artist', genre: 'rap'})
+		.end(function(err, res){
+			res.should.have.lengthOf(n);
+			done();
+		})
+
+	});
+	it('should return no more than 20 results if no limit is specified', function(done){
+		chai.request(server)
+		.get('/search')
+		.query({limit:''})
+		.query({type: 'artist', genre: 'rap'})
+		.end(function(err, res){
+			res.should.have.length.below(21);
+			done();
+		})
+
+	});
+
 	describe('Artists', function(){
 
-		it('should get a list of Artists by genre', function(done){
-			chai.request(server)
-			.get('/search')
-			.query({type: 'artist', genre: 'pop'})
-			.end(function(err, res){
-				res.should.have.status(200);
-				done();
-			})
-		});
-		it('should get a list of Artists that match a name', function(done){
-			chai.request(server)
-			.get('/search')
-			.query({type: 'artist', name: 'Nas'})			
-			.end(function(err, res){
-				res.should.have.status(200);
-				done();
-			})
-
-		});
+		it('should get a list of Artists by genre')//, function(done){
+			// chai.request(server)
+			// .get('/search/artists')
+			// .query({type: 'artist', genre: 'pop'})
+			// .end(function(err, res){
+			// 	res.should.have.status(200);
+			// 	done();
+			// })
+		// });
+		it('should get a list of Artists that match a name');
 	});
 
 	describe('Albums', function(){
+		// search/albums
 		it('should get a list of albums by genre');
 		it('should get a list of albums that match a certain name');
 	})
