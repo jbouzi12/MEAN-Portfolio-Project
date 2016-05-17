773e7d84e861cb4bf63a6e79f2f03e248d9eed73
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-05-16 23:06:36 -0400

    remove route test that did not deal with search functionality

--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -6,15 +6,32 @@ var chai 		= require('chai'),
 chai.use(chaiHttp);
 
 describe('Search', function(){
-	it('should get a list of Artists by genre', function(done){
-		chai.request(server)
-		.get('/artists/genre')
-		.end(function(err, res){
-			res.should.have.status(200);
-			done();
-		})
+	describe('Artists', function(){
+
+		it('should get a list of Artists by genre', function(done){
+			chai.request(server)
+			// .get('/search')
+			.get('/artists/:genre')
+			.end(function(err, res){
+				res.should.have.status(200);
+				done();
+			})
+		});
+		it('should get a list of Artists that match a name', function(done){
+			chai.request(server)
+			// .get('/search')
+			.get('/artists/:name')
+			.end(function(err, res){
+				res.should.have.status(200);
+				done();
+			})
+
+		});
 	});
-	it('should get a list of Artists by name');
-	it('should get a list of albums by genre');
-	it('should get a list of albums by name');
+
+	describe('Albums', function(){
+		it('should get a list of albums by genre');
+		it('should get a list of albums that match a certain name');
+		it('should ')		
+	})
 })
