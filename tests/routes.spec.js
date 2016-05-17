439e7b922fe77ea93ea3864c9475d7f067048d62
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-05-16 22:54:16 -0400

    add basic test for /artists/:genre

--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -6,7 +6,14 @@ var chai 		= require('chai'),
 chai.use(chaiHttp);
 
 describe('Search', function(){
-	it('should get a list of Artists by genre');
+	it('should get a list of Artists by genre', function(done){
+		chai.request(server)
+		.get('/artists/genre')
+		.end(function(err, res){
+			res.should.have.status(200);
+			done();
+		})
+	});
 	it('should get a list of Artists by name');
 	it('should get a list of albums by genre');
 	it('should get a list of albums by name');
