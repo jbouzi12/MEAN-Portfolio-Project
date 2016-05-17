d94638071b50af055d8b99f2adfe5a12b7a1d66e
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-05-16 23:16:45 -0400

    add middleware to enable cors

--- server/app.js
+++ server/app.js
@@ -8,6 +8,11 @@ var express		= require('express'),
 app.use(bodyParser.json()); // parse application/json 
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+app.use(function(req, res, next){ // enable CORS (will refactor into middleware later)
+	res.header('Access-Control-Allow-Origin', "*");
+	res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
 app.listen(port, function(){
 	console.log('listening on port: ', port);
