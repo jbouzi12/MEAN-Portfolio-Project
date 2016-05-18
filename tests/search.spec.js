240d90b1bbde53a32e55dc49075ab6adf76e0018
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-05-17 23:18:30 -0400

    update tests to acommodate new api structure

--- tests/search.spec.js
+++ tests/search.spec.js
@@ -1,6 +1,7 @@
 var chai 		= require('chai'),
 	chaiHttp	= require('chai-http'),
 	server 		= require('../server/app'),
+	expect		= chai.expect,
 	should 		= chai.should();
 
 chai.use(chaiHttp);
@@ -19,19 +20,18 @@ describe('Search', function(){
 		var n = 5;
 		chai.request(server)
 		.get('/search')
-		.query({limit:n})
-		.query({type: 'artist', genre: 'rap'})
+		.query({type: 'artist', limit: n})
 		.end(function(err, res){
-			res.should.have.lengthOf(n);
+			expect(res).to.have.lengthOf(5)
 			done();
-		})
+		});
 
 	});
 	it('should return no more than 20 results if no limit is specified', function(done){
 		chai.request(server)
 		.get('/search')
 		.query({limit:''})
-		.query({type: 'artist', genre: 'rap'})
+		.query({type: 'artist'})
 		.end(function(err, res){
 			res.should.have.length.below(21);
 			done();
@@ -39,23 +39,4 @@ describe('Search', function(){
 
 	});
 
-	describe('Artists', function(){
-
-		it('should get a list of Artists by genre')//, function(done){
-			// chai.request(server)
-			// .get('/search/artists')
-			// .query({type: 'artist', genre: 'pop'})
-			// .end(function(err, res){
-			// 	res.should.have.status(200);
-			// 	done();
-			// })
-		// });
-		it('should get a list of Artists that match a name');
-	});
-
-	describe('Albums', function(){
-		// search/albums
-		it('should get a list of albums by genre');
-		it('should get a list of albums that match a certain name');
-	})
 })
