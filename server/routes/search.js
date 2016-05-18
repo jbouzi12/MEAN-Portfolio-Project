f4f867b973a8138e12dff364b5668a3235755710
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-05-18 16:05:37 -0400

    alter response data in search route to include results & search type

--- server/routes/search.js
+++ server/routes/search.js
@@ -7,9 +7,16 @@ var express		= require('express'),
 
 
 router.get('/search', function(req, res){
+	var type = req.query.type + 's';
 	Spotify.search(req, res)
 		.then(function(results){
-			res.send(results.artists.items);
+			var data = {
+				music: results[type].items,
+				type: type
+			}
+			console.log(data.music.length);
+
+			res.send(data);
 		})
 		.fail(function(err){
 			res.send(err);
