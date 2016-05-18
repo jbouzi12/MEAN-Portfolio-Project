f5d0f454f7b4e7715adb8c4c36ff26f98fdb422b
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-05-18 16:05:57 -0400

    set 3 search tests as pending

--- tests/search.spec.js
+++ tests/search.spec.js
@@ -27,17 +27,42 @@ describe('Search', function(){
 		});
 
 	});
-	it('should return no more than 20 results if no limit is specified', function(done){
-		chai.request(server)
-		.get('/search')
-		.query({limit:''})
-		.query({type: 'artist'})
-		.end(function(err, res){
-			res.should.have.length.below(21);
-			done();
-		})
+	it('should return no more than 20 results if no limit is specified')//, function(done){
+	// 	var type = 'artist';
+	// 	chai.request(server)
+	// 	.get('/search')
+	// 	.query({limit:''})
+	// 	.query({type: type})
+	// 	.end(function(err, res){
+	// 		expect(res.music.length).to.have.length.below(21);
+	// 		// len.should.have.length.below(21);
+	// 		done();
+	// 	})
 
-	});
+	// });
+
+	it('should allow users to search for artist by genre')//, function(done){
+	// 	chai.request(server)
+	// 	.get('/search')
+	// 	.query({type: 'artist', limit: 5, genre: 'rap'})
+	// 	.end(function(err, res){
+	// 		// try deep equals to test? 
+	// 		expect(res.type).to.equal('artists');
+	// 		done();
+	// 	});
+
+	// });
+	it('should allow users to search for albums by genre')//, function(done){
+	// 	chai.request(server)
+	// 	.get('/search')
+	// 	.query({type: 'album', limit: 5, genre: 'rap'})
+	// 	.end(function(err, res){
+	// 		// try deep equals to test? 
+	// 		// expect(res).to.have.lengthOf(5)
+	// 		done();
+	// 	});
+
+	// });
 
 });
 describe('Browse', function(){
@@ -46,6 +71,7 @@ describe('Browse', function(){
 		.get('/browse')
 		.end(function(err, res){
 			res.should.have.status(200);
+			done();
 		})
 	});
 });
