72796a982eacd83a7a547bc3043d23e3850bb31c
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-05-25 17:17:17 -0400

    initialize angular app and begin to create search factory

---
+++ client/app/search/search.factory.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('topFive.searchFactory')
+        .factory('searchFactory', searchFactory);
+
+  function searchFactory(){
+    
+  };
+
+});
