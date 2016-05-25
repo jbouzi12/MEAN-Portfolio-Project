c07d1953a0ed900e3fcee03736255e5756f4bf6e
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-05-24 22:00:34 -0400

    refactor search route to return only array of album results

--- server/routes/search.js
+++ server/routes/search.js
@@ -10,13 +10,8 @@ router.get('/search', function(req, res){
 	var type = req.query.type + 's';
 	Spotify.search(req, res)
 		.then(function(results){
-			var data = {
-				music: results[type].items,
-				type: type
-			}
-			console.log(data.music.length);
 
-			res.send(data);
+			res.send(results[type].items);
 		})
 		.fail(function(err){
 			res.send(err);
