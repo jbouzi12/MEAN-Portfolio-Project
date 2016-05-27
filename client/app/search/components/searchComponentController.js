1a5dbfa8d61923a168beef5bd42578ea3c52bcf9
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-05-26 21:43:00 -0400

    create #getResults function to process search results

--- client/app/search/components/searchComponentController.js
+++ client/app/search/components/searchComponentController.js
@@ -7,7 +7,17 @@
   searchComponentCtrl.$inject = ['searchFactory'];
 
   function searchComponentCtrl(searchFactory){
-    
+    var vm = this;
+    vm.options = {
+      type:'',
+      query:'',
+      limit:20
+    }
+    vm.getResults = getResults;
+
+    function getResults(resultsArr){
+      vm.onSearch(resultsArr);
+    }
   }
 
 })();
