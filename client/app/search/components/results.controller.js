8ded3f0d03edc962462bf60d4f78a46554bf5ec5
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-05-30 17:51:41 -0400

    create results controller

---
+++ client/app/search/components/results.controller.js
@@ -0,0 +1,19 @@
+(function(){
+  'use strict';
+
+  angular.module('topFive.search')
+    .controller('resultsComponentController', resultsCtrl);
+
+  function searchComponentCtrl() {
+    // var vm = this;
+    //
+    // vm.results = results;
+    //
+    // vm.getResults = getResults;
+    //
+    // function getResults(resultsArr){
+    //   vm.onSearch(resultsArr);
+    // }
+  }
+
+})();
