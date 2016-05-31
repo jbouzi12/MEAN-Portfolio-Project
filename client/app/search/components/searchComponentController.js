729c3890aad6e79887e064537973ccf4544fc7a9
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-05-31 11:36:33 -0400

    inject search factory into search componentCtrl and add TODO comments

--- client/app/search/components/searchComponentController.js
+++ client/app/search/components/searchComponentController.js
@@ -4,10 +4,9 @@
   angular.module('topFive.search')
     .controller('searchComponentController', searchComponentCtrl);
 
-  // searchComponentCtrl.$inject = ['searchFactory'];
+  searchComponentCtrl.$inject = ['searchFactory'];
 
-  // function searchComponentCtrl(searchFactory){
-  function searchComponentCtrl() {
+  function searchComponentCtrl(searchFactory){
     var vm = this;
     vm.options = {
       type:'',
@@ -17,8 +16,13 @@
     vm.getResults = getResults;
 
     function getResults(resultsArr){
+      // should capture results array and set it to "results"
+      // object in home ctrl
       vm.onSearch(resultsArr);
     }
+    // create search function that calls search factory
+    // grabs results and calls vm.getResults to calls onSearch
+    // REFACTOR: get rid of #getResults and call vm.onSearch directly
   }
 
 })();
