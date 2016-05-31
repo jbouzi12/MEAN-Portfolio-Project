6618b32ce9d3f5c3c8017fa5f1b3719a842ae4f5
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-05-31 11:36:10 -0400

    inject ngResource into main app module

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
 
-  angular.module('topFive', ['topFive.home', 'topFive.search']);
+  angular.module('topFive', ['topFive.home', 'topFive.search', 'ngResource']);
 
 })();
