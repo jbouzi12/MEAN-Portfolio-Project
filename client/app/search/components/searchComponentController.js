9a46b9814f8cf5ef5e11530b0ea67ce3acdfe412
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-06-01 13:23:45 -0400

    create search function in searchCtrl that calls searchFactory

--- client/app/search/components/searchComponentController.js
+++ client/app/search/components/searchComponentController.js
@@ -13,22 +13,17 @@
       query:'',
       limit:20
     };
-    vm.getResults = getResults;
+    // vm.getResults = getResults;
     vm.search = search;
 
-    function search(options){
+    return vm;
+    function search(){
       searchFactory.search(vm.options).then(function(data){
         console.log('search results', data);
         // vm.onSearch(data);
       });
     }
 
-
-    function getResults(resultsArr){
-      // should capture results array and set it to "results"
-      // object in home ctrl
-      vm.onSearch(resultsArr);
-    }
     // create search function that calls search factory
     // grabs results and calls vm.getResults to calls onSearch
     // REFACTOR: get rid of #getResults and call vm.onSearch directly
