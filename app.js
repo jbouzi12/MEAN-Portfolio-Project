9a8d74545daf40d865dadae77c0fde19fc00bc71
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-06-01 13:21:37 -0400

    reorder middleware and comment out middleware for rendering index page

--- app.js
+++ app.js
@@ -13,21 +13,22 @@ var express		= require('express'),
 // Routers
 var searchRouter = require('./server/routes/search');
 
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/client'));
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-// app.use(bodyParser.__dirname+'/client'({ extended: false }));
-
 app.use(function(req, res, next){ // enable CORS (will refactor into middleware later)
 	res.header('Access-Control-Allow-Origin', "*");
 	res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/client'));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+// app.use(bodyParser.__dirname+'/client'({ extended: false }));
+
 // Routes
-app.use(function(req, res){
-	res.sendFile(path.join(__dirname+'/client/index.html'));
-});
+// app.use('/',function(req, res){
+// 	res.sendFile(path.join(__dirname+'/client/index.html'));
+// });
 app.use('/', searchRouter);
 
 var server = app.listen(port, function(){
