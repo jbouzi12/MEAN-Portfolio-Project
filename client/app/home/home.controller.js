cb342f23549ccb3fc91d7edae0de7dae9c12c991
Jensen Bouzi <jensen.bouzi@gmail.com>
2016-06-01 13:37:33 -0400

    create onSearch function on homeCtrl and results array to grab search results from user queries

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -8,6 +8,12 @@
     var vm = this;
     vm.name = "My Top 5";
     vm.description = "Discover new albums, document your favorites, and enjoy with your peers";
+    vm.onSearch = onSearch;
+    vm.results = [];
+
+    function onSearch(results){
+      vm.results = results;
+    }
   }
 
 })();
